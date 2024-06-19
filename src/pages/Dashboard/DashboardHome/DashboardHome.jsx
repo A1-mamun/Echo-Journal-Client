65ba24268177a00a2be39508261b86de6ddf6e12
A1-mamun <1154almamun@gmail.com>
2024-06-19 15:21:07 +0600

    Dashboard home title added

--- src/pages/Dashboard/DashboardHome/DashboardHome.jsx
+++ src/pages/Dashboard/DashboardHome/DashboardHome.jsx
@@ -1,17 +1,18 @@
+import { Helmet } from "react-helmet-async";
 import MostViewedArticle from "./MostViewedArticle";
 import PublisherArticle from "./PublisherArticle";
 import PublisherViewed from "./PublisherViewed";
 
 const DashboardHome = () => {
-  //   console.log(publisher);
-
   return (
     <div className="space-y-7">
+      <Helmet>
+        <title>EchoJournal | Dashboard Home</title>
+      </Helmet>
       <div className="flex flex-col lg:flex-row justify-between gap-5">
         <PublisherArticle />
         <MostViewedArticle />
       </div>
-
       <PublisherViewed />
     </div>
   );
