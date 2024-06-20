305ae9458017793c4378538193a56ce3fa02951d
A1-mamun <1154almamun@gmail.com>
2024-06-20 15:25:37 +0600

    Home page statistics section react countup integrate

--- src/pages/Home/Statistics/Statistics.jsx
+++ src/pages/Home/Statistics/Statistics.jsx
@@ -4,6 +4,8 @@ import { GiClick } from "react-icons/gi";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosCommon from "../../../Hooks/useAxiosCommon";
 import Loader from "../../../components/shared/Loader";
+import CountUp from "react-countup";
+import Heading from "../../../components/shared/Heading";
 
 const Statistics = () => {
   const axiosCommon = useAxiosCommon();
@@ -18,6 +20,7 @@ const Statistics = () => {
   if (isLoading) return <Loader></Loader>;
   return (
     <section className="p-6 my-6 dark:bg-gray-100 dark:text-gray-800">
+      <Heading title={"Our User Statistics"}></Heading>
       <div className="container grid grid-cols-1 gap-6 mx-auto sm:grid-cols-2 xl:grid-cols-4">
         <div className="flex p-4 space-x-4 rounded-lg md:space-x-6 dark:bg-gray-50 dark:text-gray-800">
           <div className="flex justify-center p-2 align-middle rounded-lg sm:p-4 dark:bg-violet-600">
@@ -25,7 +28,11 @@ const Statistics = () => {
           </div>
           <div className="flex flex-col justify-center align-middle">
             <p className="text-3xl font-semibold leading-none">
-              {statistics?.users}
+              <CountUp
+                enableScrollSpy={true}
+                start={0}
+                end={statistics?.users}
+              />
             </p>
             <p className="capitalize">Users</p>
           </div>
@@ -36,7 +43,11 @@ const Statistics = () => {
           </div>
           <div className="flex flex-col justify-center align-middle">
             <p className="text-3xl font-semibold leading-none">
-              {statistics?.normalUsers}
+              <CountUp
+                enableScrollSpy={true}
+                start={0}
+                end={statistics?.normalUsers}
+              />
             </p>
             <p className="capitalize">Normal Users</p>
           </div>
@@ -47,7 +58,11 @@ const Statistics = () => {
           </div>
           <div className="flex flex-col justify-center align-middle">
             <p className="text-3xl font-semibold leading-none">
-              {statistics?.users - statistics?.normalUsers}
+              <CountUp
+                enableScrollSpy={true}
+                start={0}
+                end={statistics?.users - statistics?.normalUsers}
+              />
             </p>
             <p className="capitalize">Premium Users</p>
           </div>
@@ -58,7 +73,11 @@ const Statistics = () => {
           </div>
           <div className="flex flex-col justify-center align-middle">
             <p className="text-3xl font-semibold leading-none">
-              {statistics?.totalView}
+              <CountUp
+                enableScrollSpy={true}
+                start={0}
+                end={statistics?.totalView}
+              />
             </p>
             <p className="capitalize">Total view</p>
           </div>
