75f29e78c72df5c0514d4c97232e3668d5429336
A1-mamun <1154almamun@gmail.com>
2024-06-20 15:53:04 +0600

    Home page plans section update

--- src/pages/Home/Plans/Plans.jsx
+++ src/pages/Home/Plans/Plans.jsx
@@ -12,7 +12,7 @@ const Plans = () => {
                 <span className="text-6xl font-bold">Free</span>
               </div>
               <p className="mt-3 leading-relaxed dark:text-gray-600">
-                Free trial for 1 month
+                Free trial for normal user
               </p>
               <ul className="flex-1 mb-6 dark:text-gray-600">
                 <li className="flex mb-2 space-x-2">
@@ -28,7 +28,7 @@ const Plans = () => {
                       clipRule="evenodd"
                     ></path>
                   </svg>
-                  <span>Aenean quis</span>
+                  <span>Can read normal articles</span>
                 </li>
                 <li className="flex mb-2 space-x-2">
                   <svg
@@ -43,22 +43,7 @@ const Plans = () => {
                       clipRule="evenodd"
                     ></path>
                   </svg>
-                  <span>Morbi semper</span>
-                </li>
-                <li className="flex mb-2 space-x-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    className="flex-shrink-0 w-6 h-6 dark:text-violet-600"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                  <span>Tristique enim nec</span>
+                  <span>Limit article post ( 1 article )</span>
                 </li>
               </ul>
               <Link to="/subscribe">
@@ -69,15 +54,13 @@ const Plans = () => {
           <div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0">
             <div className="flex flex-grow flex-col p-6 space-y-6 rounded shadow sm:p-8 dark:bg-gray-50">
               <div className="space-y-2">
-                <h4 className="text-2xl font-bold">Pro</h4>
+                <h4 className="text-2xl font-bold">Premium testing</h4>
                 <span className="text-6xl font-bold">
-                  $24
-                  <span className="text-sm tracking-wide">/month</span>
+                  $5
+                  <span className="text-sm tracking-wide">/minuet</span>
                 </span>
               </div>
-              <p className="leading-relaxed">
-                Morbi cursus ut sapien sit amet consectetur.
-              </p>
+              <p className="leading-relaxed">For premium testing purpose</p>
               <ul className="space-y-2 dark:text-gray-600">
                 <li className="flex items-start space-x-2">
                   <svg
@@ -107,7 +90,7 @@ const Plans = () => {
                       clipRule="evenodd"
                     ></path>
                   </svg>
-                  <span>Fusce sem ligula</span>
+                  <span>Can enjoy all normal & premium articles</span>
                 </li>
                 <li className="flex items-start space-x-2">
                   <svg
@@ -122,37 +105,7 @@ const Plans = () => {
                       clipRule="evenodd"
                     ></path>
                   </svg>
-                  <span>Curabitur dictum</span>
-                </li>
-                <li className="flex items-start space-x-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    className="flex-shrink-0 w-6 h-6 dark:text-violet-600"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                  <span>Duis odio eros</span>
-                </li>
-                <li className="flex items-start space-x-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    className="flex-shrink-0 w-6 h-6 dark:text-violet-600"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                  <span>Vivamus ut lectus ex</span>
+                  <span>Unlimited article post</span>
                 </li>
               </ul>
               <Link to="/subscribe">
@@ -163,14 +116,14 @@ const Plans = () => {
           <div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0">
             <div className="flex flex-grow flex-col p-6 space-y-6 rounded shadow sm:p-8 dark:bg-gray-50">
               <div className="space-y-2">
-                <h4 className="text-2xl font-bold">Team</h4>
+                <h4 className="text-2xl font-bold">Pro</h4>
                 <span className="text-6xl font-bold">
-                  $72
+                  $10
                   <span className="text-sm tracking-wide">/month</span>
                 </span>
               </div>
               <p className="leading-relaxed dark:text-gray-600">
-                Phasellus ultrices bibendum nibh in vehicula.
+                For professionals
               </p>
               <ul className="space-y-2 dark:text-gray-600">
                 <li className="flex items-start space-x-2">
@@ -201,7 +154,7 @@ const Plans = () => {
                       clipRule="evenodd"
                     ></path>
                   </svg>
-                  <span>Fusce sem ligula</span>
+                  <span>5 users at a time</span>
                 </li>
                 <li className="flex items-start space-x-2">
                   <svg
@@ -216,7 +169,7 @@ const Plans = () => {
                       clipRule="evenodd"
                     ></path>
                   </svg>
-                  <span>Curabitur dictum</span>
+                  <span>Can enjoy all articles</span>
                 </li>
                 <li className="flex items-start space-x-2">
                   <svg
@@ -231,22 +184,7 @@ const Plans = () => {
                       clipRule="evenodd"
                     ></path>
                   </svg>
-                  <span>Duis odio eros</span>
-                </li>
-                <li className="flex items-start space-x-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    className="flex-shrink-0 w-6 h-6 dark:text-violet-600"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                  <span>Vivamus ut lectus ex</span>
+                  <span>Unlimited post</span>
                 </li>
               </ul>
               <Link to="/subscribe">
